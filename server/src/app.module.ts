4248299196d18b33242ca735c13aec2a3fb5b8a4
MakoskiVictor <makoski.ed@gmail.com>
2024-03-21 11:45:27 -0300

    import CharactersModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CharactersModule } from './characters/characters.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    CharactersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
