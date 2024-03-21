5d1f370b9fa0a922fd8f1577949b5c431a0c7edc
MakoskiVictor <makoski.ed@gmail.com>
2024-03-21 15:05:28 -0300

    create wands entity

--- server/src/wands/entities/wand.entity.ts
+++ server/src/wands/entities/wand.entity.ts
@@ -1,1 +1,24 @@
-export class Wand {}
+import {
+  Column,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Wand {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar', nullable: true })
+  wood: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  core: string;
+
+  @Column({ type: 'float', nullable: true })
+  length: number;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
