04eab81a33005e48dd7988300ac8db906a572112
MakoskiVictor <makoski.ed@gmail.com>
2024-03-21 11:50:34 -0300

    create characters entity

---
+++ server/src/characters/entities/character.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Character {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  name: string;
+
+  @Column({ type: 'enum', default: null, nullable: true })
+  gender: 'male' | 'female' | null;
+
+  @Column({ type: 'date', default: null, nullable: true })
+  date_of_birth: string;
+
+  @Column({ type: 'boolean', nullable: false })
+  wizard: boolean;
+
+  @Column({ type: 'varchar', nullable: true })
+  patronus: string;
+
+  @Column({ type: 'boolean', nullable: false })
+  hogwarts_student: boolean;
+
+  @Column({ type: 'boolean', nullable: false })
+  hogwarts_staff: boolean;
+
+  @Column({ type: 'varchar', nullable: true })
+  actor: string;
+
+  @Column({ type: 'boolean', nullable: false })
+  alive: boolean;
+
+  @Column({ type: 'varchar', nullable: true })
+  image: string;
+}
