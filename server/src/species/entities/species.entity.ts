668a88a36e7b82778891a3a63c2aec1363423012
MakoskiVictor <makoski.ed@gmail.com>
2024-03-21 14:51:25 -0300

    create species entity

--- server/src/species/entities/species.entity.ts
+++ server/src/species/entities/species.entity.ts
@@ -1,1 +1,13 @@
-export class Species {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Species {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column('varchar')
+  name: string;
+
+  @Column('varchar')
+  description: string;
+}
