f24bf2d0be8635cfeb20add5b0ccdac61950cacd
MakoskiVictor <makoski.ed@gmail.com>
2024-03-21 14:49:27 -0300

    create house entity

--- server/src/houses/entities/house.entity.ts
+++ server/src/houses/entities/house.entity.ts
@@ -1,1 +1,13 @@
-export class House {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class House {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({ type: 'varchar' })
+  description: string;
+}
