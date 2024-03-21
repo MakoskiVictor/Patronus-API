4891ada714b0d395a41d5014c423fdadf101a16d
MakoskiVictor <makoski.ed@gmail.com>
2024-03-21 17:27:44 -0300

    create barrel

---
+++ server/src/alternate_names/index.ts
@@ -0,0 +1,5 @@
+export * from './alternate_names.controller';
+export * from './alternate_names.module';
+export * from './alternate_names.service';
+export * from './dto';
+export * from './entities';
