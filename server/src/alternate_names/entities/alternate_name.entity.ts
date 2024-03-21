0ea792e045d4089c427fa5d7cce5f3e2e3d787bc
MakoskiVictor <makoski.ed@gmail.com>
2024-03-21 14:46:31 -0300

    create alternat_names entity

--- server/src/alternate_names/entities/alternate_name.entity.ts
+++ server/src/alternate_names/entities/alternate_name.entity.ts
@@ -1,1 +1,10 @@
-export class AlternateName {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class AlternateName {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', array: true })
+  name: string[];
+}
