617fe69fc73505c9bca50c283a7645f16f05dceb
MakoskiVictor <makoski.ed@gmail.com>
2024-03-21 14:53:34 -0300

    create spell entity

--- server/src/spells/entities/spell.entity.ts
+++ server/src/spells/entities/spell.entity.ts
@@ -1,1 +1,13 @@
-export class Spell {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Spell {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column('varchar')
+  name: string;
+
+  @Column('varchar')
+  description: string;
+}
