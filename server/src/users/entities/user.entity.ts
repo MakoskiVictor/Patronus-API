da3b0af85c7d82369428340d811ce9c5f096ef2d
MakoskiVictor <makoski.ed@gmail.com>
2024-03-21 14:59:15 -0300

    create user entity

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', nullable: false, unique: true })
+  username: string;
+
+  @Column({ type: 'varchar', nullable: false, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  password: string;
+
+  @Column({ type: 'enum', default: 'user', nullable: true })
+  rol: 'user' | 'admin';
+}
