9a716564a25c51bffe4e5450048e53020f8e7168
MakoskiVictor <makoski.ed@gmail.com>
2024-03-22 17:38:14 -0300

    set Global Pipes

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Set prefix
   app.setGlobalPrefix('api/v1');
+
+  // Set Auto-Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(5000);
 }
 bootstrap();
