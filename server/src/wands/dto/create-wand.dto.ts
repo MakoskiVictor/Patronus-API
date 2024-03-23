c564ae05be54e46ea7259a0db327ad15e0bb7373
MakoskiVictor <makoski.ed@gmail.com>
2024-03-23 07:26:25 -0300

    add validators to create wand

--- server/src/wands/dto/create-wand.dto.ts
+++ server/src/wands/dto/create-wand.dto.ts
@@ -1,1 +1,24 @@
-export class CreateWandDto {}
+import {
+  IsDecimal,
+  IsEmpty,
+  IsNumber,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateWandDto {
+  @IsEmpty()
+  @IsString()
+  @MaxLength(50)
+  wood: string;
+
+  @IsEmpty()
+  @IsString()
+  @MaxLength(50)
+  core: string;
+
+  @IsEmpty()
+  @IsNumber()
+  @IsDecimal({ decimal_digits: '2' })
+  length: number;
+}
