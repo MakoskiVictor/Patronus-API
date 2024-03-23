b954868f3ba5ec57eb88a13f541a57924556f2a6
MakoskiVictor <makoski.ed@gmail.com>
2024-03-23 07:08:56 -0300

    add validation to create house

--- server/src/houses/dto/create-house.dto.ts
+++ server/src/houses/dto/create-house.dto.ts
@@ -1,1 +1,12 @@
-export class CreateHouseDto {}
+import { IsEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateHouseDto {
+  @IsEmpty()
+  @IsString()
+  @MaxLength(50)
+  name: string;
+
+  @IsEmpty()
+  @IsString()
+  descriprion: string;
+}
