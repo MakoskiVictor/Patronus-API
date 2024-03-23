40ab9874185afd81620513fd47034f736106323f
MakoskiVictor <makoski.ed@gmail.com>
2024-03-23 07:12:49 -0300

    create validation create spell

--- server/src/spells/dto/create-spell.dto.ts
+++ server/src/spells/dto/create-spell.dto.ts
@@ -1,1 +1,13 @@
-export class CreateSpellDto {}
+import { IsEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateSpellDto {
+  @IsEmpty()
+  @IsString()
+  @MaxLength(50)
+  name: string;
+
+  @IsEmpty()
+  @IsString()
+  @MaxLength(255)
+  description: string;
+}
