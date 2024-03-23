3a650cb75a66827196e233856fdc4f52f2c29364
MakoskiVictor <makoski.ed@gmail.com>
2024-03-23 20:55:54 -0300

    create dto

--- server/src/houses/dto/update-house.dto.ts
+++ server/src/houses/dto/update-house.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateHouseDto } from './create-house.dto';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 
-export class UpdateHouseDto extends PartialType(CreateHouseDto) {}
+export class UpdateHouseDto extends PartialType(CreateHouseDto) {
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+}
