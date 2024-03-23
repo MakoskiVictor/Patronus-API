3d92f823cf73e2abd50db4ed2b523696a84303d5
MakoskiVictor <makoski.ed@gmail.com>
2024-03-23 07:10:42 -0300

    add validations to create species

--- server/src/species/dto/create-species.dto.ts
+++ server/src/species/dto/create-species.dto.ts
@@ -1,1 +1,12 @@
-export class CreateSpeciesDto {}
+import { IsEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateSpeciesDto {
+  @IsEmpty()
+  @IsString()
+  @MaxLength(50)
+  name: string;
+
+  @IsEmpty()
+  @IsString()
+  deacription: string;
+}
