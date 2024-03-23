b7b8612b2d8d6eb1f09f00aef7dc6d911fe560c0
MakoskiVictor <makoski.ed@gmail.com>
2024-03-23 17:35:46 -0300

    add create, findOne and findAll fn

--- server/src/houses/houses.service.ts
+++ server/src/houses/houses.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateHouseDto } from './dto/create-house.dto';
 import { UpdateHouseDto } from './dto/update-house.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { House } from './entities';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class HousesService {
-  create(createHouseDto: CreateHouseDto) {
-    return 'This action adds a new house';
+  constructor(
+    @InjectRepository(House) private houseRepository: Repository<House>,
+  ) {}
+
+  // CREATE HOUSE
+  async create(createHouseDto: CreateHouseDto) {
+    // Search if the House alredy exists
+
+    const houseFound = await this.houseRepository.findOne({
+      where: {
+        name: createHouseDto.name,
+      },
+    });
+
+    if (houseFound) {
+      throw new HttpException('The House alredy exist!', HttpStatus.CONFLICT);
+    }
+
+    // If no exists, create one
+    const newHouse = new House();
+    newHouse.name = createHouseDto.name;
+    newHouse.description = createHouseDto.description;
+    return await this.houseRepository.save(newHouse);
   }
 
-  findAll() {
-    return `This action returns all houses`;
+  async findAll() {
+    // Seach the Houses
+    return this.houseRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} house`;
+  async findOne(uuid: string) {
+    const findHouse = await this.houseRepository.findOne({
+      where: {
+        id: uuid,
+      },
+    });
+
+    if (!findHouse) {
+      throw new HttpException('House not found', HttpStatus.NOT_FOUND);
+    }
+    return findHouse;
   }
 
-  update(id: number, updateHouseDto: UpdateHouseDto) {
+  async update(id: number, updateHouseDto: UpdateHouseDto) {
     return `This action updates a #${id} house`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} house`;
   }
 }
