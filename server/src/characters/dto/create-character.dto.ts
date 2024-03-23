fd2e0986a8254d1888a5adc42c4e051e10ba70da
MakoskiVictor <makoski.ed@gmail.com>
2024-03-23 07:44:07 -0300

    add validators to create character

--- server/src/characters/dto/create-character.dto.ts
+++ server/src/characters/dto/create-character.dto.ts
@@ -1,1 +1,54 @@
-export class CreateCharacterDto {}
+import {
+  IsBoolean,
+  IsDate,
+  IsEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateCharacterDto {
+  @IsEmpty()
+  @IsString()
+  @MaxLength(50)
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  gender?: string;
+
+  @IsOptional()
+  @IsDate()
+  date_of_birth?: Date;
+
+  @IsEmpty()
+  @IsBoolean()
+  wizard: boolean;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  patronus?: string;
+
+  @IsEmpty()
+  @IsBoolean()
+  hogwarts_student: boolean;
+
+  @IsEmpty()
+  @IsBoolean()
+  hogwarts_staff: boolean;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  actor?: string;
+
+  @IsEmpty()
+  @IsBoolean()
+  alive: boolean;
+
+  @IsOptional()
+  @IsUrl()
+  image?: string;
+}
