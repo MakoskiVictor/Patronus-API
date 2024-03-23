2ec5b2db262f8151ef8ab8a40e32ba36e8c70864
MakoskiVictor <makoski.ed@gmail.com>
2024-03-23 07:22:09 -0300

    add validations create user

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmpty()
+  @IsString()
+  @MaxLength(50)
+  username: string;
+
+  @IsEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsEmpty()
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  rol?: string;
+}
