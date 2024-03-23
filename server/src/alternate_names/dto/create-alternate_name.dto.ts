0319952d5ecb31551cc5af25fa0e40b90252c342
MakoskiVictor <makoski.ed@gmail.com>
2024-03-23 07:05:07 -0300

    cretae dto create alter name

--- server/src/alternate_names/dto/create-alternate_name.dto.ts
+++ server/src/alternate_names/dto/create-alternate_name.dto.ts
@@ -1,1 +1,8 @@
-export class CreateAlternateNameDto {}
+import { IsString, IsNotEmpty, MaxLength } from 'class-validator';
+
+export class CreateAlternateNameDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  name: string;
+}
