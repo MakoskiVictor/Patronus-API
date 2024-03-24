d963f6de441a0cd407b68e946e5d244d9239880e
MakoskiVictor <makoski.ed@gmail.com>
2024-03-24 09:41:26 -0300

    add validations

--- server/src/species/dto/update-species.dto.ts
+++ server/src/species/dto/update-species.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSpeciesDto } from './create-species.dto';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 
-export class UpdateSpeciesDto extends PartialType(CreateSpeciesDto) {}
+export class UpdateSpeciesDto extends PartialType(CreateSpeciesDto) {
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+}
