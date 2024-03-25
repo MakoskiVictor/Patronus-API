40c64cf87a13602d4b840a7be71d91c82568621b
MakoskiVictor <makoski.ed@gmail.com>
2024-03-25 11:33:05 -0300

    add create fn

--- server/src/spells/spells.service.ts
+++ server/src/spells/spells.service.ts
@@ -1,26 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateSpellDto } from './dto/create-spell.dto';
 import { UpdateSpellDto } from './dto/update-spell.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Spell } from './entities';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SpellsService {
-  create(createSpellDto: CreateSpellDto) {
-    return 'This action adds a new spell';
+  constructor(
+    @InjectRepository(Spell) private spellsRepository: Repository<Spell>,
+  ) {}
+  async create(createSpellDto: CreateSpellDto) {
+    const findSpell = await this.spellsRepository.findOne({
+      where: {
+        name: createSpellDto.name,
+      },
+    });
+    if (findSpell) {
+      throw new HttpException('This Spell alredy exist!', HttpStatus.CONFLICT);
+    }
+    return this.spellsRepository.save(createSpellDto);
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all spells`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} spell`;
   }
 
-  update(id: number, updateSpellDto: UpdateSpellDto) {
+  async update(id: number, updateSpellDto: UpdateSpellDto) {
     return `This action updates a #${id} spell`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} spell`;
   }
 }
