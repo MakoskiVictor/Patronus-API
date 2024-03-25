c6442af1c584366c114a823b53025c1ea2133cd0
MakoskiVictor <makoski.ed@gmail.com>
2024-03-25 17:59:31 -0300

    add update wand

--- server/src/wands/dto/update-wand.dto.ts
+++ server/src/wands/dto/update-wand.dto.ts
@@ -1,4 +1,26 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateWandDto } from './create-wand.dto';
+import {
+  IsDecimal,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
-export class UpdateWandDto extends PartialType(CreateWandDto) {}
+export class UpdateWandDto extends PartialType(CreateWandDto) {
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  wood?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  core?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @IsDecimal({ decimal_digits: '2' })
+  length?: number;
+}
