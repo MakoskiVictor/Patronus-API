e9e1468c8c602b1dd1bfd8cc483200e56e2decc6
MakoskiVictor <makoski.ed@gmail.com>
2024-03-25 09:22:52 -0300

    add update alternatename dto

--- server/src/alternate_names/dto/update-alternate_name.dto.ts
+++ server/src/alternate_names/dto/update-alternate_name.dto.ts
@@ -1,4 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAlternateNameDto } from './create-alternate_name.dto';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 
-export class UpdateAlternateNameDto extends PartialType(CreateAlternateNameDto) {}
+export class UpdateAlternateNameDto extends PartialType(
+  CreateAlternateNameDto,
+) {
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  name: string;
+}
