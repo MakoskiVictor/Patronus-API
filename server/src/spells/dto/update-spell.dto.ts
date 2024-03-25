e3e7591bfc32b4debed4659f1b04983d13aa70f4
MakoskiVictor <makoski.ed@gmail.com>
2024-03-25 10:20:22 -0300

    add update dto

--- server/src/spells/dto/update-spell.dto.ts
+++ server/src/spells/dto/update-spell.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSpellDto } from './create-spell.dto';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 
-export class UpdateSpellDto extends PartialType(CreateSpellDto) {}
+export class UpdateSpellDto extends PartialType(CreateSpellDto) {
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  description?: string;
+}
